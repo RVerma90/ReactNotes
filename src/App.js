16f534ab24357d18ad7d1fc18dabb485813c4426
RVerma90 <irahulverma90@gmail.com>
2017-08-16 23:54:54 +0100

    Firebase integrated, just add config details

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import Note from './Note/Note';
 import NoteForm from './NoteForm/NoteForm';
+import { DB_CONFIG } from './Config/config';
+import firebase from 'firebase/app';
+import 'firebase/database';
 import './App.css';
 
 class App extends Component {
@@ -9,38 +11,68 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.addNote = this.addNote.bind(this);
+        this.removeNote = this.removeNote.bind(this);
+
+        this.app = firebase.initializeApp(DB_CONFIG);
+        this.database = this.app.database().ref().child('notes');
+
         // Setup the React State of components
         this.state = {
-          notes: [
-            { id: 1, noteContent: "Note 1 here!"},
-            { id: 2, noteContent: "Note 2 here!"}
-          ]
+          notes: []
         }
     }
 
-    addNote(note) {
-      // Push the Note onto the notes array.
+    componentWillMount() {
       const previousNotes = this.state.notes;
-      // Will replace hardcoded strings with Firebase
-      previousNotes.push({ id: previousNotes.length + 1, noteContent: note});
 
-      this.setState({
-        notes: previousNotes
+      // DataSnapshot from Firebase
+      this.database.on('child_added', snap => {
+        previousNotes.push({
+          id: snap.key,
+          noteContent: snap.val().noteContent
+        })
+
+        this.setState({
+          notes: previousNotes
+        })
+      })
+
+      this.database.on('child_removed', snap => {
+        for(var i=0; i < previousNotes.length; i++){
+          if(previousNotes[i].id === snap.key) {
+            // i is the index to delete, by 1
+            previousNotes.splice(i, 1);
+          }
+        }
+
+        this.setState({
+          notes: previousNotes
+        })
       })
     }
 
+    addNote(note) {
+      this.database.push().set({ noteContent: note });
+    }
+
+    removeNote(noteId) {
+      this.database.child(noteId).remove();
+    }
 
     render() {
         return (
             <div className="notesWrapper">
               <div className="notesHeader">
-                <div className="heading">React & Firebase To-Do List</div>
+                <div className="heading">To-Do List</div>
               </div>
               <div className="notesBody">
               {
                 this.state.notes.map((note) => {
                   return (
-                  <Note noteContent={note.noteContent} noteId={note.id} key={note.id} />
+                  <Note noteContent={note.noteContent}
+                    noteId={note.id}
+                    key={note.id}
+                    removeNote={this.removeNote} />
                   )
                 })
               }
